cae565eb9384fe6391c3a2ae7ebe8775cba25a03
Valentin Brosseau <c4software@gmail.com>
2018-02-10 19:02:13 +0100

    Changement dans le theme du blog

--- params.py
+++ params.py
@@ -11,7 +11,7 @@ settings = {"site_title":"Un peu de tout, un peu de rien...",
 			
 			"static_settings":"./static/",
 			"theme":"./theme/valentin_new/",
-			"blog_settings":{"index_page":"index","nbchar_resume":100,"nb_per_page":7},
+			"blog_settings":{"index_page":"index","nbchar_resume":300,"nb_per_page":7},
 			"scss_settings":{"files":[('main.scss','main.css'),('pygments.scss','pygments.css')],"path":"./theme/valentin/static/styles/"},
 
 			"diff_build":True,
