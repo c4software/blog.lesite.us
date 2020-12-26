c8eb06ac28db135054c110408044eb3330a56d64
Valentin Brosseau <c4software@gmail.com>
2020-12-26 16:08:58 +0100

    Update Yasb and stuff around to enable lazy image loading

--- params.py
+++ params.py
@@ -1,27 +1,27 @@
-settings = {"site_title":"Un peu de tout, un peu de rien...", 
-			"author":"Valentin Brosseau",
-			"url":"http://blog.lesite.us",
-			"input":"./source/",
-			"output":"./output/",
+settings = {"site_title": "Un peu de tout, un peu de rien...",
+            "author": "Valentin Brosseau",
+            "url": "http://blog.lesite.us",
+            "input": "./source/",
+            "output": "./output/",
 
-			"title_as_name":True,
+            "title_as_name": True,
 
-			"plugins":['sitemap','blog',"theme","static"],
-			#"plugins":['sitemap','blog','pyscss',"theme","static"],
-			
-			"static_settings":"./static/",
-			"theme":"./theme/valentin_new/",
-			"blog_settings":{"index_page":"index","nbchar_resume":300,"nb_per_page":7},
-			"scss_settings":{"files":[('main.scss','main.css'),('pygments.scss','pygments.css')],"path":"./theme/valentin/static/styles/"},
+            "plugins": ['img_lazy_loading', 'sitemap', 'blog', "theme", "static"],
+            # "plugins":['sitemap','blog','pyscss',"theme","static"],
 
-			"diff_build":True,
-			"lastbuild_file":"./output/.lastbuild",
-			"diff_build_db":"./output/.diff_build_db",
+            "static_settings": "./static/",
+            "theme": "./theme/valentin_new/",
+            "blog_settings": {"index_page": "index", "nbchar_resume": 300, "nb_per_page": 7},
+            "scss_settings": {"files": [('main.scss', 'main.css'), ('pygments.scss', 'pygments.css')], "path": "./theme/valentin/static/styles/"},
 
-			"links":(
-					    ('Archives', '/archives.html'),
-					    ('Moi', 'http://valentinbrosseau.lesite.us/'),
-					    ('Twitter', 'http://twitter.com/c4software'),
-					    ('Flux RSS', 'feeds/all.atom.xml')
-			        )
-			}
+            "diff_build": True,
+            "lastbuild_file": "./output/.lastbuild",
+            "diff_build_db": "./output/.diff_build_db",
+
+            "links": (
+                ('Archives', '/archives.html'),
+                ('Moi', '/author/valentin-brosseau.html'),
+                ('Twitter', 'https://twitter.com/c4software'),
+                ('Flux RSS', 'feeds/all.atom.xml')
+            )
+            }
